c75c7386475d7133c75ea463e0f3b78c3c73a0d5
Alex <alexandralaudon@gmail.com>
2015-03-25 12:15:07 -0600

    Finished user_spec.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  has_many :posts
+  has_many :comments
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 
 describe User do
-  before :each do
-    @user2 = User.create(first_name: "Alex", last_name: "laudon", email: "pwadams@me.com", password: "password")
-  end
   describe 'validations' do
+    before :each do
+      @user2 = User.create(first_name: "Alex", last_name: "laudon", email: "whatever@me.com", password: "password")
+    end
 
     it 'requires that first name is valid' do
       user1 = User.create(last_name: "Adams", email: "pwadams@me.com", password: "password")
@@ -22,9 +22,8 @@ describe User do
     it 'requires that email is valid' do
       user1 = User.create(first_name: "Pamela", last_name: "Adams", email: "pwadams@me.com", password: "password")
       user3 = User.create(first_name: "Alex", last_name: "laudon", password: "password")
-
       expect(user1.errors.present?).to eq(false)
-      expect(@user2.errors.present?).to eq(true)
+      expect(@user2.errors.present?).to eq(false)
       expect(user3.errors.present?).to eq(true)
     end
 
@@ -36,14 +35,17 @@ describe User do
   end
 
   describe 'associations' do
-    @post = Post.create(title: 'Hello world, whaz up?', user_id: @user2.id)
+    before :each do
+      @user2 = User.create(first_name: "Alex", last_name: "laudon", email: "pwadams@me.com", password: "password")
+      @post = Post.create(title: 'Hello world, whaz up?', user_id: @user2.id)
+    end
 
     it 'User has_many Posts' do
-      expect(@user2.posts).to eq(Array(post))
+      expect(@user2.posts).to eq(Array(@post))
     end
 
     it 'User has_many Comments' do
-      comment = Comment.create(body: "we're creating apps, man!", post_id: @post.id, user_id: @user.id)
+      comment = Comment.create(body: "we're creating apps, man!", post_id: @post.id, user_id: @user2.id)
       expect(@user2.comments).to eq(Array(comment))
     end
   end
